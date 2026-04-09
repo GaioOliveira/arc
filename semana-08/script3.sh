86e55a3d811c1c7377b90edd2c58c1af34594539
GaioOliveira <gaio.oliveira@ifsp.edu.br>
2026-04-08 22:09:12 -0300

    Create script3.sh

---
+++ semana-08/script3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Definição de variáveis e atribuição 
+autor="Gaio B. Oliveira"
+data=$(date +%x) 
+# O comando entre $() será executado e
+# seu resultado guardado na variável
+
+echo "Script desenvolvido por $autor"
+echo "Data de criação: $data"
